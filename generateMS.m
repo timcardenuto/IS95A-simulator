30fa4fa3cfef8adb9b33b4d9ea7d1931575a2ddd
timcardenuto <timcardenuto@gmail.com>
2017-06-04 20:53:45 -0400

    Added copyright

--- generateMS.m
+++ generateMS.m
@@ -21,6 +21,21 @@
 %           call_start_time
 %           call_end_time
 %
+% License
+%    Copyright (C) 2017 Tim Cardenuto
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program. If not, see <http://www.gnu.org/licenses/>.
 %#########################################################################
 
 function MS = generateMS(num_calls, call_arrival_mean, call_duration_mean, ...
